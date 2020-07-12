c2490fce0c17e7238cda26aa499d225c59e77531
Siddharth Rajput <rajputsiddharth18@gmail.com>
2020-07-12 23:13:49 +0530

    Till Variable

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -7,14 +7,22 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Terminal.ClearScreen();
-        print("Hello Console");
-        Terminal.WriteLine("HACK THIS TERMINAL {**}");
-        Terminal.WriteLine("Press 1 for your Local Host Library...");
-        Terminal.WriteLine("Press 2 for your Server Host Library...");
-        Terminal.WriteLine("Press 3 for your Doing nothing");
+        ShowMainMenu("SIDDHARTH");
     }
 
+
+    void ShowMainMenu(string Name)
+    {
+        string name = "Siddharth Rajput";
+        Terminal.ClearScreen();
+        Terminal.WriteLine("HI" + name);
+        print("Hello Console");
+        Terminal.WriteLine("HACK THIS TERMINAL {**}");
+        Terminal.WriteLine("Press 1 for your Local Host Library");
+        Terminal.WriteLine("Press 2 for your Server Host Library");
+        Terminal.WriteLine("Press 3 for your Doing nothing");
+        Terminal.WriteLine("ENTER YOUR CHOICE:");
+    }
     // Update is called once per frame
     void Update()
     {
