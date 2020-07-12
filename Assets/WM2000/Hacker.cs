d0c7bb8688e38110e8fad8298e762941b3a97788
Siddharth Rajput <rajputsiddharth18@gmail.com>
2020-07-12 21:03:58 +0530

    Set Uped IDE

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.ClearScreen();
+        print("Hello Console");
+        Terminal.WriteLine("HACK THIS TERMINAL {**}");
+        Terminal.WriteLine("Press 1 for your Local Host Library...");
+        Terminal.WriteLine("Press 2 for your Server Host Library...");
+        Terminal.WriteLine("Press 3 for your Doing nothing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
