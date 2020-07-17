dd0e966579a4843be88a724a30a36f3b1bb6fc53
Siddharth-rajput2201 <rajputsiddharth18@gmail.com>
2020-07-17 15:21:24 +0530

    added password

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -14,55 +14,85 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu()
     {
+        Currentscreen = Screen.Mainmenu; 
         Terminal.ClearScreen();
         Terminal.WriteLine("HI");
-        print("Hello Console");
         Terminal.WriteLine("HACK THIS TERMINAL {**}");
         Terminal.WriteLine("Press 1 for your Local Host Library");
         Terminal.WriteLine("Press 2 for your Server Host Library");
-        Terminal.WriteLine("Press 3 for your Doing nothing");
+        Terminal.WriteLine("Type menu for menu");
         Terminal.WriteLine("ENTER YOUR CHOICE:");
     }
     // Update is called once per frame
 
     int Level;
+    String Password;
 
     enum Screen { Mainmenu , Password , Win }
     Screen Currentscreen = Screen.Mainmenu;
 
     void OnUserInput(string Input )
     {
-        if(Input == "menu")
+        Terminal.ClearScreen();
+        if (Input == "menu")
         {
             ShowMainMenu();
         }
 
-        else if(Input == "1")
+        else if (Currentscreen == Screen.Mainmenu)
+        {
+            runmainmenu(Input);
+        }
+
+        else if (Currentscreen == Screen.Password)
+        {
+            passwordcheck(Input);
+        }
+        
+    }
+
+    void runmainmenu(string Input)
+    {
+        if (Input == "1")
         {
             Level = 1;
+            Password = "hack";
             startgame(Level);
         }
 
-        else if (Input == "2")
+        if (Input == "2")
         {
             Level = 2;
+            Password = "hack";
             startgame(Level);
         }
 
-        else if(Input == "007")
+        else if (Input == "007")
         {
             Terminal.WriteLine("HEY!BOND STAY IN LIMITS!ENTER ONLY LEVEL");
         }
 
-        else
-        {
-            Terminal.WriteLine("ENTER A VALID CHOICE ");
-        }
+       
     }
 
     void startgame(int Level)
     {
-        Terminal.WriteLine("You Have Choosen " + Level);
+        Currentscreen = Screen.Password;
+        //Terminal.ClearScreen();
+        Terminal.WriteLine("You Have Choosen Level : " + Level);
+        Terminal.WriteLine("Enter The Password : ");
+    }
+
+    void passwordcheck(String Input)
+    {
+        if(Input == Password)
+        {
+            Terminal.WriteLine("YOU HACKED THE TERMINAL!");
+        }
+        else
+        {
+            Terminal.WriteLine("WRONG PASSWORD");
+        }
     }
 
     void Update()
